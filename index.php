451503f7a651daec12e19bb98c5ea43a9faafca6
Leonel Nardelli <ricosur@gmail.com>
2017-11-06 00:45:04 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="css/estilos.css">
+      <link rel="stylesheet" href="https://necolas.github.io/normalize.css/7.0.0/normalize.css">
+      <script src="http://code.jquery.com/jquery-latest.js"></script>
+   </head>
+
+   <body>
+      <div class="container">
+         <header>
+            <div class="header">
+               <nav>
+                  <ul class="menu">
+                     <li><a href="#">Inicio</a></li>
+                     <li><a href="#">Tienda</a></li>
+                     <li><a href="#">Productos</a></li>
+                     <li><a href="#">Presupuesto Online</a></li>
+                     <li><a href="#">Trabajos</a></li>
+                     <li><a href="#">Contacto</a></li>
+                  </ul>
+                     <div class="search-bar">
+                        <a href="#" class=""></a>
+                        <input type="text" id="bar" name="" value="">
+                     </div>
+               </nav>
+            </div>
+         </header>
+         <section>
+
+         </section>
+         <footer>
+
+         </footer>
+      </div>
+   </body>
+</html>
